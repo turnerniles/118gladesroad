f8e60b9720e6e7b33ba523fc782204a2c7d6ba50
Niles Turner <turnerniles@gmail.com>
2020-10-28 16:21:25 -0500

    prod build

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
 
-; jQuery = global.jQuery = require("/Users/nturner/dev/single-property-real-estate-website/node_modules/jquery/dist/jquery.js");
+; jQuery = global.jQuery = require("/Users/nilesturner/src/118gladesroad/node_modules/jquery/dist/jquery.js");
 Tether = global.Tether = require("tether");
 ; var __browserify_shim_require__=require;(function browserifyShim(module, define, require) {
 /*!
@@ -3544,7 +3544,7 @@ var Popover = function ($) {
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 
-},{"/Users/nturner/dev/single-property-real-estate-website/node_modules/jquery/dist/jquery.js":3,"tether":4}],2:[function(require,module,exports){
+},{"/Users/nilesturner/src/118gladesroad/node_modules/jquery/dist/jquery.js":3,"tether":4}],2:[function(require,module,exports){
 (function (global){
 
 ; jQuery = global.jQuery = require("jquery");
